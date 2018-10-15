89a674aa0ed919b2f2dbcc29975e8c4143e968d6
Maquiner <39957598+Maquiner@users.noreply.github.com>
2018-10-15 14:35:12 +0100

    Add files via upload

---
+++ WikiDive/background.js
@@ -0,0 +1,20 @@
+chrome.runtime.onInstalled.addListener(function() {
+    // Replace all rules ...
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      // With a new rule ...
+      chrome.declarativeContent.onPageChanged.addRules([
+        {
+          // That fires when a page's URL contains a 'g' ...
+          conditions: [
+            new chrome.declarativeContent.PageStateMatcher({
+              pageUrl: { urlContains: 'wikipedia.org' },
+            })
+          ],
+          // And shows the extension's page action.
+          actions: [ new chrome.declarativeContent.ShowPageAction({
+              display: "popup.html", 
+            }) ]
+        }
+      ]);
+    });
+  });
